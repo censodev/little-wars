4fe0449d36095f6e7b1343d7390aad8c59bd611a
Phuong Bui <51000027+censodev@users.noreply.github.com>
2021-03-19 16:03:16 +0700

    p10

---
+++ Assets/Scripts/AudioEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioEnum
+{
+    Win,
+    Lose,
+    Buy,
+    Up,
+    Sword,
+    Range,
+    Guard,
+    Bullet,
+    Magic,
+}
